2bf04822e55787e3a2a27dfd51ecb4b090a96512
Ajay Raj <najayraj.16.cse@anits.edu.in>
2018-02-18 19:29:49 +0530

    :+1:

--- generator.py
+++ generator.py
@@ -1,25 +1,25 @@
 class Combinations:
-    def __init__(self, n , r):
-        self.arr = [1] * int(r)
-            self.init_value = -1
-                self.i = -1
-                self.recursion_count = 0
-        
-        def generate_combinations(self, n, r):
-            self.i = self.i + 1
-                
-                if r == 0:
-                    print ( self.arr )
-                else:
-                    if self.i != 0:
-                        self.init_value = self.arr[self.i-1]
-                        
-                        self.arr[self.i] = self.init_value + 1
-                        while self.arr[self.i] < int(n):
-                            self.generate_combinations(n, r - 1)
-                                self.arr[self.i] = self.arr[self.i] + 1
-                
-            self.i = self.i - 1
+	def __init__(self, n , r):
+		self.arr = [1] * int(r)
+		self.init_value = 0
+		self.i = -1
+		self.recursion_count = 0
+
+	def generate_combinations(self, n, r):
+		self.i = self.i + 1
+
+		if r == 0:
+			print ( self.arr )
+		else:
+			if self.i != 0:
+				self.init_value = self.arr[self.i-1]
+
+			self.arr[self.i] = self.init_value + 1
+			while self.arr[self.i] <= int(n):
+				self.generate_combinations(n, r - 1)
+				self.arr[self.i] = self.arr[self.i] + 1
+
+		self.i = self.i - 1
 
 n = int(input())
 r = int(input())
